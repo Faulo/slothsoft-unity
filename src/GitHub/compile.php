22ac33b1b0e67bec29e17acd281cf45dea73e3a6
Daniel Schulz <faulolio@gmail.com>
2022-07-11 17:19:05 +0200

    Add strict typing

--- src/GitHub/compile.php
+++ src/GitHub/compile.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 namespace Slothsoft\Unity\GitHub;
 
 $queue = __DIR__ . '/queue.bat';
--- src/GitHub/process-queue.php
+++ src/GitHub/process-queue.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 namespace Slothsoft\Unity\GitHub;
 
 $queueFile = __DIR__ . '/queue.bat';
