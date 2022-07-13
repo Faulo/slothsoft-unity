6131afc8749d18e622cb6a95a90ad0426994e130
Daniel Schulz <faulolio@gmail.com>
2022-07-13 03:24:55 +0200

    Add tests

---
+++ tests/Assets/ProjectBuildParameterFilterTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Unity\Assets;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ProjectBuildParameterFilterTest
+ *
+ * @see ProjectBuildParameterFilter
+ *
+ * @todo auto-generated
+ */
+class ProjectBuildParameterFilterTest extends TestCase {
+
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(ProjectBuildParameterFilter::class), "Failed to load class 'Slothsoft\Unity\Assets\ProjectBuildParameterFilter'!");
+    }
+}
---
+++ tests/ProcessRunnerTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Unity;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ProcessRunnerTest
+ *
+ * @see ProcessRunner
+ *
+ * @todo auto-generated
+ */
+class ProcessRunnerTest extends TestCase {
+
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(ProcessRunner::class), "Failed to load class 'Slothsoft\Unity\ProcessRunner'!");
+    }
+}
