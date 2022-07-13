b7312fbd838667b63ab5c4b22dcc5184684cdd47
Daniel Schulz <faulolio@gmail.com>
2022-07-13 16:45:53 +0200

    Retire ChunkWriterFromGenerator

--- src/Assets/HubHelpBuilder.php
+++ src/Assets/HubHelpBuilder.php
@@ -2,13 +2,14 @@
 declare(strict_types = 1);
 namespace Slothsoft\Unity\Assets;
 
+use Slothsoft\Core\IO\Writable\Adapter\ChunkWriterFromGenerator;
 use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
 use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\ChunkWriterResultBuilder;
-use Slothsoft\Unity\UnityHub;
 use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\NullResultBuilder;
+use Slothsoft\Unity\UnityHub;
 
 class HubHelpBuilder implements ExecutableBuilderStrategyInterface {
 
--- src/Assets/HubInstallBuilder.php
+++ src/Assets/HubInstallBuilder.php
@@ -2,15 +2,16 @@
 declare(strict_types = 1);
 namespace Slothsoft\Unity\Assets;
 
+use Slothsoft\Core\IO\Writable\Adapter\ChunkWriterFromGenerator;
 use Slothsoft\Core\IO\Writable\Delegates\ChunkWriterFromChunksDelegate;
 use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
 use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\ChunkWriterResultBuilder;
+use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\NullResultBuilder;
 use Slothsoft\Unity\UnityHub;
 use Generator;
-use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\NullResultBuilder;
 
 class HubInstallBuilder implements ExecutableBuilderStrategyInterface {
 
