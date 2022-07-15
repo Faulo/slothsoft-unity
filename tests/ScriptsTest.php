173132b1d7f51d12b17059529eadf69b6a0c252e
Daniel Schulz <faulolio@gmail.com>
2022-07-15 11:36:41 +0200

    Add tests for commands

--- tests/ScriptsTest.php
+++ tests/ScriptsTest.php
@@ -7,20 +7,36 @@ use Symfony\Component\Process\Process;
 
 class ScriptsTest extends TestCase {
 
-    public function testUnityTests(): void {
+    /**
+     *
+     * @dataProvider validBinaries
+     */
+    public function testUnityTests(string $script): void {
         $process = new Process([
             PHP_BINARY,
-            'scripts/unity-tests'
+            "scripts/$script"
         ]);
 
         $code = $process->run();
         $result = $process->getOutput();
         $errors = $process->getErrorOutput();
 
-        $this->assertEquals('', $errors, 'Calling unity-tests failed! Command:' . PHP_EOL . $process->getCommandLine());
+        $this->assertEquals('', $errors, "Calling $script failed! Command:" . PHP_EOL . $process->getCommandLine());
 
-        $this->assertEquals(0, $code, 'Calling unity-tests failed! Command:' . PHP_EOL . $process->getCommandLine());
+        $this->assertEquals(0, $code, "Calling $script failed! Command:" . PHP_EOL . $process->getCommandLine());
 
-        $this->assertStringContainsString('composer exec unity-tests', $result, 'Calling unity-tests failed! Command:' . PHP_EOL . $process->getCommandLine());
+        $this->assertStringContainsString("composer exec $script", $result, "Calling $script failed! Command:" . PHP_EOL . $process->getCommandLine());
+    }
+
+    public function validBinaries(): iterable {
+        yield 'unity-tests' => [
+            'unity-tests'
+        ];
+        yield 'unity-build' => [
+            'unity-build'
+        ];
+        yield 'unity-method' => [
+            'unity-method'
+        ];
     }
 }
