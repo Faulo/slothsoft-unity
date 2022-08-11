5f715af513c6bfc75f03bfecd9d7878c433deac0
Daniel Schulz <faulolio@gmail.com>
2022-08-11 20:38:51 +0200

    Try fix license file

--- src/UnityEditor.php
+++ src/UnityEditor.php
@@ -71,10 +71,9 @@ class UnityEditor {
         }
 
         $log = $this->execute('-quit', '-createManualActivationFile')->getOutput();
-        $position = strpos($log, self::LICENSE_CREATED);
-        if ($position !== false) {
-            $log = explode("\n", substr($log, $position + strlen(self::LICENSE_CREATED)), 2);
-            $log = trim($log[0]);
+        $match = [];
+        if (preg_match('~(Unity_v[^\s]+\.alf)~', $log, $match)) {
+            $log = trim($match[1]);
             if (is_file($log)) {
                 $this->hub->prepareLicense($log);
             }
