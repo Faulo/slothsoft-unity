f8b536d957194eea68fc9c4ffa79a093732d05de
Daniel Schulz <faulolio@gmail.com>
2022-08-13 15:43:50 +0200

    Fix directories

--- src/Assets/Package/ExecutableBase.php
+++ src/Assets/Package/ExecutableBase.php
@@ -20,6 +20,9 @@ use Throwable;
 abstract class ExecutableBase implements ExecutableBuilderStrategyInterface {
 
     /** @var string */
+    protected string $packageDirectory;
+
+    /** @var string */
     protected string $workspace;
 
     /** @var UnityPackage */
@@ -35,15 +38,16 @@ abstract class ExecutableBase implements ExecutableBuilderStrategyInterface {
     private ?ExecutionError $error;
 
     protected function parseArguments(FarahUrlArguments $args): void {
+        $this->packageDirectory = $args->get('package');
         $this->workspace = $args->get('workspace');
     }
 
     protected function validate(): ?ExecutionError {
-        if (! is_dir($this->workspace)) {
-            return ExecutionError::Error('AssertDirectory', "Workspace '{$this->workspace}' is not a directory!");
+        if (! is_dir($this->packageDirectory)) {
+            return ExecutionError::Error('AssertDirectory', "Workspace '{$this->packageDirectory}' is not a directory!");
         }
 
-        $this->workspace = realpath($this->workspace);
+        $this->packageDirectory = realpath($this->packageDirectory);
 
         $hub = UnityHub::getInstance();
 
@@ -51,17 +55,19 @@ abstract class ExecutableBase implements ExecutableBuilderStrategyInterface {
             return ExecutionError::Error('AssertHub', "Failed to find Unity Hub!");
         }
 
-        $this->package = $hub->findPackage($this->workspace);
+        $this->package = $hub->findPackage($this->packageDirectory);
 
         if (! $this->package) {
-            return ExecutionError::Error('AssertPackage', "Workspace '{$this->workspace}' does not contain a Unity package!");
+            return ExecutionError::Error('AssertPackage', "Workspace '{$this->packageDirectory}' does not contain a Unity package!");
         }
 
         if (! $this->package->ensureEditorIsInstalled()) {
             return ExecutionError::Error('AssertEditor', "Editor installation for package '{$this->package}' failed!");
         }
 
-        $this->project = $this->package->createEmptyProject(temp_dir(__NAMESPACE__));
+        $this->project = $this->package->createEmptyProject($this->workspace);
+
+        $this->workspace = $this->project->getProjectPath();
 
         if (! $this->project->ensureEditorIsInstalled()) {
             return ExecutionError::Error('AssertEditor', "Editor installation for package '{$this->package}' failed!");
