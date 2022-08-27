99a598aacaffeef443e9b65c13962c66c8c1d02c
Daniel Schulz <faulolio@gmail.com>
2022-08-27 17:29:41 +0200

    Update docs

---
+++ tests/Assets/Project/ModuleExecutableTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Unity\Assets\Project;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ModuleExecutableTest
+ *
+ * @see ModuleExecutable
+ *
+ * @todo auto-generated
+ */
+class ModuleExecutableTest extends TestCase {
+
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(ModuleExecutable::class), "Failed to load class 'Slothsoft\Unity\Assets\Project\ModuleExecutable'!");
+    }
+}
