49e24c98106f3b405cedf8c8ecb06c99c66e079d
Daniel Schulz <faulolio@gmail.com>
2022-09-17 19:00:06 +0200

    Update docs

---
+++ tests/Assets/Project/VersionExecutableTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Unity\Assets\Project;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * VersionExecutableTest
+ *
+ * @see VersionExecutable
+ *
+ * @todo auto-generated
+ */
+class VersionExecutableTest extends TestCase {
+
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(VersionExecutable::class), "Failed to load class 'Slothsoft\Unity\Assets\Project\VersionExecutable'!");
+    }
+}
---
+++ tests/Assets/Project/VersionParametersTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Unity\Assets\Project;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * VersionParametersTest
+ *
+ * @see VersionParameters
+ *
+ * @todo auto-generated
+ */
+class VersionParametersTest extends TestCase {
+
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(VersionParameters::class), "Failed to load class 'Slothsoft\Unity\Assets\Project\VersionParameters'!");
+    }
+}
