d1af8bfbabed68adfb6f5cc003afdcc5906c5411
Daniel Schulz <faulolio@gmail.com>
2023-03-06 12:08:37 +0100

    Fix encoding issue under windows

--- src/Steam/AppBuild.php
+++ src/Steam/AppBuild.php
@@ -22,6 +22,11 @@ class AppBuild {
      *            build output folder for build logs and build cache files
      */
     public function __construct(string $appId, string $description, string $contentPath, string $buildPath) {
+        $this->fixEncoding($appId);
+        $this->fixEncoding($description);
+        $this->fixEncoding($contentPath);
+        $this->fixEncoding($buildPath);
+
         $this->data['AppID'] = $appId;
         $this->data['Desc'] = $description;
         $this->data['ContentRoot'] = $contentPath;
@@ -40,6 +45,11 @@ class AppBuild {
      *            include all subfolders
      */
     public function addDepot(string $depotId, string $localPath = '*', string $depotPath = '.', string $recursive = '1'): void {
+        $this->fixEncoding($depotId);
+        $this->fixEncoding($localPath);
+        $this->fixEncoding($depotPath);
+        $this->fixEncoding($recursive);
+
         $this->data['Depots'][$depotId] = [
             "FileMapping" => [
                 "LocalPath" => $localPath,
@@ -55,7 +65,22 @@ class AppBuild {
      *            name of the branch to set live
      */
     public function setLive(string $branch): void {
+        $this->fixEncoding($branch);
+
         $this->data['SetLive'] = $branch;
+    }
+
+    const SUPPORTED_ENCODINGS = [
+        'UTF-8',
+        'Windows-1252',
+        'ISO-8859-1'
+    ];
+
+    private function fixEncoding(string &$value) {
+        $encoding = mb_detect_encoding($value, self::SUPPORTED_ENCODINGS);
+        if ($encoding and $encoding !== 'UTF-8') {
+            $value = mb_convert_encoding($value, 'UTF-8', $encoding);
+        }
     }
 
     public function __toString(): string {
--- tests/Steam/AppBuildTest.php
+++ tests/Steam/AppBuildTest.php
@@ -16,4 +16,47 @@ class AppBuildTest extends TestCase {
     public function testClassExists(): void {
         $this->assertTrue(class_exists(AppBuild::class), "Failed to load class 'Slothsoft\Unity\Steam\AppBuild'!");
     }
+
+    /**
+     *
+     * @dataProvider validEncodings
+     */
+    public function testToStringIsUTF8(string $encoding): void {
+        $appId = '123';
+        $description = 'Hellö';
+        $contentPath = '/var/server/öäü';
+        $buildPath = 'C:\\Webserver\\workspace@tmp';
+        $branch = 'mäin';
+
+        $appId2 = mb_convert_encoding($appId, $encoding, 'UTF-8');
+        $description2 = mb_convert_encoding($description, $encoding, 'UTF-8');
+        $contentPath2 = mb_convert_encoding($contentPath, $encoding, 'UTF-8');
+        $buildPath2 = mb_convert_encoding($buildPath, $encoding, 'UTF-8');
+        $branch2 = mb_convert_encoding($branch, $encoding, 'UTF-8');
+
+        $app = new AppBuild($appId2, $description2, $contentPath2, $buildPath2);
+        $app->setLive($branch2);
+
+        $content = (string) $app;
+        $this->assertEquals('UTF-8', mb_detect_encoding($content));
+        $this->assertStringContainsString($appId, $content);
+        $this->assertStringContainsString($description, $content);
+        $this->assertStringContainsString($contentPath, $content);
+        $this->assertStringContainsString($buildPath, $content);
+        $this->assertStringContainsString($branch, $content);
+    }
+
+    public function validEncodings(): iterable {
+        return [
+            [
+                'UTF-8'
+            ],
+            [
+                'Windows-1252'
+            ],
+            [
+                'ISO-8859-1'
+            ]
+        ];
+    }
 }
