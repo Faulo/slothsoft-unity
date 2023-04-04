c65a6ad66c3e3ebef501ccb4342c412c5532514c
Daniel Schulz <faulolio@gmail.com>
2023-04-04 19:16:00 +0200

    Add ensureEditorIsInstalled

--- src/Assets/Package/InstallExecutable.php
+++ src/Assets/Package/InstallExecutable.php
@@ -24,10 +24,6 @@ class InstallExecutable extends PackageExecutableBase {
             mkdir($this->workspace, 0777, true);
         }
         $this->workspace = realpath($this->workspace);
-
-        if (! $this->package->ensureEditorIsLicensed($this->workspace)) {
-            throw ExecutionError::Error('AssertLicense', "Editor for package '{$this->package}' is not licensed! Visit https://license.unity3d.com/manual for manual activation of a license for editor version '{$this->package->getEditorVersion()}'.");
-        }
     }
 
     protected function getExecutablePackage(): string {
@@ -39,6 +35,14 @@ class InstallExecutable extends PackageExecutableBase {
     }
 
     protected function createResultDocument(): ?DOMDocument {
+        if (! $this->package->ensureEditorIsInstalled()) {
+            throw ExecutionError::Error('AssertEditor', "Editor installation for package '{$this->package}' failed!");
+        }
+
+        if (! $this->package->ensureEditorIsLicensed($this->workspace)) {
+            throw ExecutionError::Error('AssertLicense', "Editor for package '{$this->package}' is not licensed! Visit https://license.unity3d.com/manual for manual activation of a license for editor version '{$this->package->getEditorVersion()}'.");
+        }
+
         $this->project = $this->package->createEmptyProject($this->workspace);
 
         $this->workspace = $this->project->getProjectPath();
