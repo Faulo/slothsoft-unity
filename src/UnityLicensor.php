ed6d552f8b4cab11b433d2b466f46657c9ec4470
Daniel Schulz <faulolio@gmail.com>
2025-04-22 02:13:35 +0200

    fix: submit

--- src/UnityLicensor.php
+++ src/UnityLicensor.php
@@ -150,7 +150,7 @@ class UnityLicensor {
 
                     $form = $input->form();
                     $form->disableValidation();
-                    $crawler = $this->browser->submit($input);
+                    $crawler = $this->browser->submit($form);
 
                     $this->log();
 
