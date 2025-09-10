7a3c6c9b5e93201f37dc7f2af3016e80a25c9370
Daniel Schulz <faulolio@gmail.com>
2025-09-10 16:00:20 +0200

    refactor: lint

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,8 +3,6 @@ declare(strict_types = 1);
 namespace Slothsoft\Unity;
 
 use Slothsoft\Farah\Module\Module;
-use Slothsoft\Unity\UnityPackage;
-
 Module::registerWithXmlManifestAndDefaultAssets('slothsoft@unity', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets');
 
 UnityPackage::setEmptyManifestFile(__DIR__ . DIRECTORY_SEPARATOR . 'manifest.json');
