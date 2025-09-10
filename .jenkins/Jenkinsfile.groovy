e2c6bd599c6868e3b7081315046b88fcd1ed56b8
Daniel Schulz <faulolio@gmail.com>
2025-09-10 16:09:08 +0200

    devops: use unity node

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -1,26 +1,30 @@
-def runTests(def versions) {
+def runTests() {
+	callShell 'composer update --prefer-lowest'
+
+	dir('.reports') {
+		deleteDir()
+	}
+
+	def report = ".reports/${version}.xml"
+
+	catchError(stageResult: 'UNSTABLE', buildResult: 'UNSTABLE', catchInterruptions: false) {
+		callShell "composer exec phpunit -- --log-junit ${report}"
+	}
+
+	if (fileExists(report)) {
+		junit report
+	}
+}
+
+def runTestsInContainer(def image, def versions) {
 	for (version in versions) {
-		def image = "faulo/farah:${version}"
+		def name = "${image}:${version}"
 
 		stage("PHP: ${version}") {
-			callShell "docker pull ${image}"
+			callShell "docker pull ${name}"
 
-			docker.image(image).inside {
-				callShell 'composer update --prefer-lowest'
-
-				dir('.reports') {
-					deleteDir()
-				}
-
-				def report = ".reports/${version}.xml"
-
-				catchError(stageResult: 'UNSTABLE', buildResult: 'UNSTABLE', catchInterruptions: false) {
-					callShell "composer exec phpunit -- --log-junit ${report}"
-				}
-
-				if (fileExists(report)) {
-					junit report
-				}
+			docker.image(name).inside {
+				runTests();
 			}
 		}
 	}
@@ -36,13 +40,29 @@ pipeline {
 		COMPOSER_PROCESS_TIMEOUT = '3600'
 	}
 	stages {
+		stage('Unity') {
+			agent {
+				label 'unity && linux'
+			}
+			steps {
+				script {
+					runTests()
+				}
+			}
+		}
 		stage('Linux') {
 			agent {
 				label 'docker && linux'
 			}
 			steps {
 				script {
-					runTests(["7.4", "8.0", "8.1", "8.2", "8.3"])
+					runTestsInContainer("faulo/farah", [
+						"7.4",
+						"8.0",
+						"8.1",
+						"8.2",
+						"8.3"
+					])
 				}
 			}
 		}
@@ -52,7 +72,13 @@ pipeline {
 			}
 			steps {
 				script {
-					runTests(["7.4", "8.0", "8.1", "8.2", "8.3"])
+					runTestsInContainer("faulo/farah", [
+						"7.4",
+						"8.0",
+						"8.1",
+						"8.2",
+						"8.3"
+					])
 				}
 			}
 		}
